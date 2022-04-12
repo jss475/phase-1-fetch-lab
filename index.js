25f947fab5f466ab3d6898f8ac19ab0c3f1b0ffd
JosephShin <joespam9999@gmail.com>
2022-04-12 12:18:04 -0400

    commit tree 3d8654809597a5e157a51a30909bf2d7f392860f

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+let jsondata
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    //jsondata=json
+    renderBooks(json)
+    find5thBook(json)
+    totalPages(json)
+    findCharac(json,3)
+    //console.log(json)
+
+    // return json
+  })
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +28,32 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  console.log()
 });
+
+
+function find5thBook(data){
+
+  const book = data[4].name
+  console.log(book)
+  return book
+}
+
+function totalPages(data){
+  let totalPages = 0
+  data.forEach(book => {
+    totalPages += parseInt(book.numberOfPages)
+  })
+  console.log(totalPages)
+  return totalPages
+}
+
+function findCharac(data,num){
+  let characterList = []
+  //collect all the characters in an array
+  data.forEach(book => characterList = characterList.concat(book.characters))
+
+  console.log(characterList[num-1])
+  return characterList[num-1]
+}
+
